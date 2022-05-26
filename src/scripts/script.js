36d26eb8fb06eac9997d0ba156fcefb6a8cc41bb
GrafVishna <grafvishna4@gmail.com>
2022-05-26 10:32:08 +0300

    html

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -3,4 +3,43 @@ $(document).ready(function () {
 		$('.burger, .menu, .menu__links').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
+	$('.card__ittems').slick({
+		infinite: true,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		dots: true,
+		arrows: false,
+		speed: 1000,
+		autoplay: true,
+		autoplayspeed: 1500,
+		touchThreshold: 10,
+		waitForAnimate: false,
+		responsive: [
+			{
+			  breakpoint: 1543,
+			  settings: {
+				slidesToShow: 4
+			  }
+			},
+			{
+			  breakpoint: 1281,
+			  settings: {
+				slidesToShow: 3
+			  }
+			},
+			{
+			  breakpoint: 918,
+			  settings: {
+				slidesToShow: 2
+			  }
+			},
+			{
+			  breakpoint: 630,
+			  settings: {
+				slidesToShow: 1,
+			  }
+			}
+		]
+	});
 });
+
